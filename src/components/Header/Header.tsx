d1c65e19db82d1f45a383bb50afb366239f06a80
Beatriz <beatrizalmeidapires@hotmail.com>
2020-04-03 16:15:27 +0100

    added profile picture

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,29 @@
 import React, { useState } from 'react';
 import './Header.css';
 // Material-UI
+
 import Grid from '@material-ui/core/Grid';
 
 interface Props {
 }
 
+
 export default function Header(props: Props): JSX.Element {
 
   return (
-    <div className="header">
-      <h2 className="app-name">Monitoring App</h2>
-      <Grid container spacing={1}>
-
-      </Grid>
-    </div>
+   <div>
+        <div className="header">
+            <div className="app-name">
+                 <h2>Monitoring App</h2>
+            </div>
+            <div className="photo-container">
+                <img src="./afonso.jpeg" className="photo"></img> 
+            </div>
+            <div className="text">
+                <h6>Afonso</h6>
+                <p>Admin</p>
+            </div>
+        </div>
+  </div>   
   );
 }
